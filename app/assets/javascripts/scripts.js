9a39c6fefb1153533d66133683ef4171948e842e
garedean <garrettdolson@gmail.com>
2015-09-29 12:15:08 -0700

    Style the flash message, set favicon

---
+++ app/assets/javascripts/scripts.js
@@ -0,0 +1,3 @@
+$(document).on('page:change', function() {
+  $('.flash').delay(1500).fadeOut(800);
+});
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"]) || User.create_with_omniauth(auth_hash)
     session[:user_id] = user.id
-    redirect_to root_path, notice: "Signed in!"
+    redirect_to root_path, notice: "Welcome, #{current_user.email}!"
   end
 
   def destroy
