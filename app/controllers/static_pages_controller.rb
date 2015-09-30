9a13cd5281cf042c94577d7b37d252896f827fb5
garedean <garrettdolson@gmail.com>
2015-09-30 14:40:51 -0700

    Debugging

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,18 +4,20 @@ class StaticPagesController < ApplicationController
   layout "sign_in", only: [:sign_in]
 
   def roster
-    puts "****************************************"
-
-    puts "Auth token: #{session[:token]}"
-    puts "User id: #{session[:user_id]}"
-    puts TeamSnap.init(:token => session[:token])
-    puts "****************************************"
+    # debugging
+    if false
+      puts "****************************************"
+      puts "Auth token: #{session[:token]}"
+      puts "User id: #{session[:user_id]}"
+      puts TeamSnap.init(:token => session[:token])
+      puts "****************************************"
+    end
 
     begin
       TeamSnap.init(:token => session[:token])
       puts "Team: #{TeamSnap::Team.find(1340251)}"
-      #@team = TeamSnap::Team.find(1344217)
-      @team = TeamSnap::Team.find(1340251)
+      @team = TeamSnap::Team.find(1344217)
+      #@team = TeamSnap::Team.find(1340251)
     rescue
       flash[:notice] = nil
       destroy_session
