2184052e8a1c6e259d7a20a10faaa5429241b68f
garedean <garrettdolson@gmail.com>
2015-09-30 12:18:42 -0700

    Include TeamSnap in StaticPagesController

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,5 @@
-#require 'TeamSnap'
-
 class StaticPagesController < ApplicationController
+  include TeamSnap
   layout "sign_in", only: [:sign_in]
 
   def roster
@@ -9,6 +8,7 @@ class StaticPagesController < ApplicationController
       #@team = TeamSnap::Team.find(1344217)
       @team = TeamSnap::Team.find(1340251)
     rescue
+      flash[:notice] = nil
       destroy_session
     end
   end
