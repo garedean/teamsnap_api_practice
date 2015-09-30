2213203e95bf75efe773964a1dddc060c56695f6
garedean <garrettdolson@gmail.com>
2015-09-30 14:18:56 -0700

    Debugging

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,19 @@
 class StaticPagesController < ApplicationController
   layout "sign_in", only: [:sign_in]
 
+  def sign_in
+    puts "==============================="
+    puts session[:test]    = "test"
+    puts session[:token]   = auth_token
+    TeamSnap.init(:token => session[:token])
+    puts TeamSnap::Team.find(1340251)
+    puts "==============================="
+  end
+
   def roster
     begin
       TeamSnap.init(:token => session[:token])
       #@team = TeamSnap::Team.find(1344217)
-      puts "==============================="
-      session[:test] = "test"
-      puts sessionn[:test]
-      puts TeamSnap::Team.find(1340251)
-      puts "==============================="
-
       @team = TeamSnap::Team.find(1340251)
     rescue
       flash[:notice] = nil
