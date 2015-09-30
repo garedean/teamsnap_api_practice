9c1a4bee046523b58d8ff3543dca2d02f263a2d0
garedean <garrettdolson@gmail.com>
2015-09-30 14:21:37 -0700

    Debugging

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,8 +5,8 @@ class StaticPagesController < ApplicationController
     puts "==============================="
     puts session[:test]
     puts session[:token]
-    TeamSnap.init(:token => session[:token])
-    puts TeamSnap::Team.find(1340251)
+    TeamSnap.init(:token => session[:token]) if session[:token]
+    #puts TeamSnap::Team.find(1340251)
     puts "==============================="
   end
 
