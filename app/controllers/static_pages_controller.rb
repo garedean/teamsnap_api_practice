5fa7a54743b3485c175ca128df5d7b669606d79d
garedean <garrettdolson@gmail.com>
2015-09-30 14:34:44 -0700

    Debugging

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,9 +4,9 @@ class StaticPagesController < ApplicationController
   def roster
     puts "****************************************"
 
-    puts "Auth token: #{auth_token}"
+    puts "Auth token: #{session[:token]}"
     puts "User id: #{session[:user_id]}"
-
+    puts TeamSnap.init(:token => session[:token])
     puts "****************************************"
 
     begin
