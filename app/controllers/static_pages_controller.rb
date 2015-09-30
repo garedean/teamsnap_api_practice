f1e2a1464b15357dd87391065955ec366b86ab21
garedean <garrettdolson@gmail.com>
2015-09-30 12:31:51 -0700

    Fix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,13 +2,18 @@ class StaticPagesController < ApplicationController
   layout "sign_in", only: [:sign_in]
 
   def roster
-    begin
-      TeamSnap.init(:token => session[:token])
-      #@team = TeamSnap::Team.find(1344217)
-      @team = TeamSnap::Team.find(1340251)
-    rescue
-      flash[:notice] = nil
-      destroy_session
+    TeamSnap.init(:token => session[:token])
+    @team = TeamSnap::Team.find(1340251)
+
+    if false
+      begin
+        TeamSnap.init(:token => session[:token])
+        #@team = TeamSnap::Team.find(1344217)
+        @team = TeamSnap::Team.find(1340251)
+      rescue
+        flash[:notice] = nil
+        destroy_session
+      end
     end
   end
 end
