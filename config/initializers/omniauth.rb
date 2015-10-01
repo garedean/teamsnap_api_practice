733472e24c4d1fc2eb9e35192479dfa5f7918a11
garedean <garrettdolson@gmail.com>
2015-10-01 10:08:46 -0700

    Remove pry statements

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :developer unless Rails.env.production?
   if Rails.env.production?
-    binding.pry
     provider :teamsnap, ENV['TEAMSNAP_KEY'], ENV['TEAMSNAP_SECRET'], scope: "read"
   else
-    binding.pry
     provider :teamsnap, ENV['DEV_TEAMSNAP_KEY'], ENV['DEV_TEAMSNAP_SECRET'], scope: "read"
   end
 end
